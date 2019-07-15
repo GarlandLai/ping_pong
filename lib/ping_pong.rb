c121ac7ecf1e25b2b4544f933edc24200e7839af
Abdellah Ramadan and Garland Lai <abdellah+garland@>
2019-07-15 11:22:43 -0700

    doing pingpong with ruby

---
+++ lib/ping_pong.rb
@@ -0,0 +1,24 @@
+class Integer
+  def ping_pong
+    x = 0
+    output_array = []
+    while (x < self)
+      x = x + 1
+      if ((x % 3) === 0) && ((x % 5) === 0)
+        output_array.push("pingpong")
+      elsif ((x % 3) === 0)
+        output_array.push("ping")
+      elsif ((x % 5) === 0)
+        output_array.push("pong")
+      else
+        output_array.push(x)
+      end
+    end
+    puts output_array
+    output_array
+  end
+end
+puts "Please enter a number:"
+input = gets.to_i
+input.ping_pong
+# puts "Your number is #{input}"
---
+++ spec/ping_pong_spec.rb
@@ -0,0 +1,24 @@
+require('rspec')
+require('ping_pong')
+require('pry')
+
+describe('#ping_pong') do
+  it("Counts from 1 to any given number") do
+    expect(2.ping_pong).to(eq([1, 2]))
+  end
+  describe('#ping_pong') do
+    it("Replaces any number divisible by 3 to ping") do
+      expect(3.ping_pong).to(eq([1, 2, "ping"]))
+    end
+    describe('#ping_pong') do
+      it("Replaces any number divisible by 5 to pong") do
+        expect(5.ping_pong).to(eq([1, 2, "ping", 4, "pong"]))
+      end
+      describe('#ping_pong') do
+        it("Replaces any number divisible by 3 and 5 to pingpong") do
+          expect(15.ping_pong).to(eq([1, 2, "ping", 4, "pong", "ping", 7, 8, "ping", "pong", 11, "ping", 13, 14, "pingpong"]))
+        end
+      end
+    end
+  end
+end
